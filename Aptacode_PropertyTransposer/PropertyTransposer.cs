f027e7b1edfc65529dd1a19f8319a6586c6cf6a6
Timothy Jones <t.jones@aptacode.com>
2019-10-08 11:39:17 +0100

    Created basic project structure

---
+++ Aptacode_PropertyTransposer/PropertyTransposer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aptacode_PropertyTransposer
+{
+    public class PropertyTransposer
+    {
+        public PropertyTransposer()
+        {
+
+        }
+    }
+}
---
+++ Aptacode_PropertyTransposer_Tests/PropertyTransposer_Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class PropertyTransposer_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+    }
+}
